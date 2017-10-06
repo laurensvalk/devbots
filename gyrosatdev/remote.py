bb64a669d5a0d31e4a400478e4b09af84bfb4a3d
Laurens Valk <laurensvalk@gmail.com>
2017-10-06 23:17:40 +0200

    Add remote and gyro damping

--- gyrosatdev/remote.py
+++ gyrosatdev/remote.py
@@ -7,7 +7,9 @@ def eprint(*args, **kwargs): # https://stackoverflow.com/a/14981125
     print(*args, file=sys.stderr, **kwargs)
 
 class gimbal:
-    def __init__(self, port):
+    def __init__(self, port, lower_limit, upper_limit):
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
         self.motor = ev3.MediumMotor('outD')
         self.set_speed(200)
         self.wait_for_stall()
@@ -22,6 +24,12 @@ class gimbal:
     def wait_for_completion(self):
         while self.motor.STATE_RUNNING in self.motor.state:
             time.sleep(0.01) 
+
+    def set_speed_safe(self, speed):
+        if(speed >= 0 and self.motor.position >= self.upper_limit) or (speed <= 0 and self.motor.position <= self.lower_limit):
+            self.stop()
+        else:
+            self.set_speed(speed)
 
     def set_speed(self, speed):
         self.motor.speed_sp = speed
@@ -45,7 +53,72 @@ class gimbal:
             time.sleep(0.01)   
 
 
-gimbal = gimbal('outD')
-gimbal.goto_motor_angle_at_speed(500,300)
-gimbal.goto_motor_angle_at_speed(0,300)
-eprint(gimbal.get_motor_angle())
+class remote:
+    def __init__(self):
+        self.irRemote          = ev3.InfraredSensor()
+        self.irRemote.mode     = self.irRemote.MODE_IR_REMOTE 
+
+    def get_button(self):
+        return self.irRemote.value(0)
+
+    def none(self):
+         return self.get_button() == 0         
+    def up_left(self):
+        return self.get_button() == 1   
+    def down_left(self):
+        return self.get_button() == 2   
+    def up_right(self):        
+        return self.get_button() == 3   
+    def down_right(self):
+        return self.get_button() == 4   
+    def top(self):
+        return self.get_button() == 9                                       
+
+class gyro:
+    def __init__(self):
+        self.sensor          = ev3.GyroSensor()
+        self.sensor.mode     = self.sensor.MODE_GYRO_RATE
+        self.calibrate_rate()
+
+    def get_rate_raw(self):
+        return self.sensor.value(0) 
+
+    def get_rate(self):
+        return self.sensor.value(0) - self.offset       
+
+    def calibrate_rate(self):
+        total = 0
+        count = 20
+        interval = 0.05
+        eprint('Calibrating Gyro')
+        for i in range(0,count):
+            total += self.get_rate_raw()
+            time.sleep(interval)
+        self.offset = round(total/count)
+        eprint("Offset: " + str(self.offset))
+
+
+
+##########################################################################
+##
+## MAIN
+##
+##########################################################################
+
+remote = remote()
+gimbal = gimbal('outD',-500,500)
+gyro = gyro()
+
+while True:
+    if remote.none():
+        gimbal.set_speed(0)
+    if remote.up_left():
+        gimbal.set_speed_safe(500)    
+    if remote.down_left():
+        gimbal.set_speed_safe(-500)  
+    if remote.up_right():
+        kp = -10
+        gimbal.set_speed_safe(gyro.get_rate()*kp)       
+    if remote.top():
+        break
+    time.sleep(0.1)
