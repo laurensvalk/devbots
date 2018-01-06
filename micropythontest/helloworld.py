f2714681a45378abb1bfd137e5af39acbcc3395b
Laurens Valk <laurensvalk@gmail.com>
2018-01-06 15:19:29 +0100

    Hello microworld

--- micropythontest/helloworld.py
+++ micropythontest/helloworld.py
@@ -1,2 +1,10 @@
-#!/usr/bin/env python3
-print("hello, world")
+#!/usr/bin/env micropython
+
+import sys
+import logging
+
+# Debug print
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+eprint("Hello, Microworld")
