5fb612d51ff6bbab959341d6dc3056df67715f3f
Laurens Valk <laurensvalk@gmail.com>
2018-01-06 18:14:12 +0100

    non blocking data

--- micropythontest/helloworld.py
+++ micropythontest/helloworld.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env micropython
-
 import sys
-import logging
+import time
+import socket
+import json
+import uselect
 
 # Debug print
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 eprint("Hello, Microworld")
+
+port = 50008
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', port))
+sock.setblocking(False)
+
+# USING POLL DOES NOT WORK. Does this need usocket?
+# poller = uselect.poll()
+# poller.register(sock, uselect.POLLIN)
+# res = poller.poll(1000)  # time in milliseconds
+# # if not res:
+# #     # s is still not ready for input, i.e. operation timed out
+
+
+for index in range(50):
+    time.sleep(0.5)
+    eprint("-----" + str(index) + "-----")
+    try:
+        data = sock.recv(2048).decode('ascii')
+        robot_broadcast_data = json.loads(data)
+        eprint(robot_broadcast_data)        
+    except:
+        eprint("no data yet")
+
+sock.close()
+   
+eprint("Goodbye, Microworld")
---
+++ micropythontest/server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import time
+import socket
+import pickle
+import json
+
+PORT = 50008
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 0))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+while True:
+    data = {'a': 1, 'b': 2}
+    message = json.dumps(data).encode('ascii')
+    sock.sendto(message, ('<broadcast>', PORT))
+    time.sleep(0.1)
